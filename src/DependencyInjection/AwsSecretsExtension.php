c353f12d53e7a1c62b2d9eb5f574b388527491b0
jmatsumura <james.matsumura@gmail.com>
2019-06-29 17:39:11 -0500

    unset many

--- src/DependencyInjection/AwsSecretsExtension.php
+++ src/DependencyInjection/AwsSecretsExtension.php
@@ -44,7 +44,10 @@ class AwsSecretsExtension extends Extension
             $configs['client_config']['credentials']['key'] === null ||
             $configs['client_config']['credentials']['secret'] === null
         ) {
-            unset($configs['client_config']['credentials']);
+            unset(
+                $configs['client_config']['credentials']['key'],
+                $configs['client_config']['credentials']['secret']
+            );
         }
 
         if ($configs['ecs_enabled']) {
